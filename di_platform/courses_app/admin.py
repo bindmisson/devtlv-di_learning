f894909db2c2834ef8f3994fe09cb72dc0e69c07
Jonathan <jonathan.spiller@gmail.com>
2020-07-17 13:43:28 +0300

    Adds Tags. Groups fields to inline admin models

--- di_platform/courses_app/admin.py
+++ di_platform/courses_app/admin.py
@@ -5,49 +5,70 @@ from courses_app.models import Course, Section, Chapter, Collection, CourseAcces
 from courses_app.models import SectionChapter, CourseSection, CollectionCourse, CourseAccessCollection
 
 
-class SectionChapterInline(admin.TabularInline):
-    model = SectionChapter
+class CollectionCourseInline(admin.TabularInline):
+    model = CollectionCourse
+    fields = ('course', 'course_tags', 'sort', )
+    readonly_fields = ['course_tags']
+
+    def course_tags(self, obj):
+      return obj.course.tags
 
 class CourseSectionInline(admin.TabularInline):
     model = CourseSection    
+    fields = ('section', 'section_tags', 'sort', )
+    readonly_fields = ['section_tags']
 
-class CollectionCourseInline(admin.TabularInline):
-    model = CollectionCourse
+    def section_tags(self, obj):
+      return obj.section.tags
+
+class SectionChapterInline(admin.TabularInline):
+    model = SectionChapter
+    fields = ('chapter', 'chapter_group', 'chapter_tags', 'sort', )
+    readonly_fields = ['chapter_tags', 'chapter_group']
+
+    def chapter_tags(self, obj):
+      return obj.chapter.tags
+    
+    def chapter_group(self, obj):
+      return obj.chapter.group
 
 class CourseAccessInline(admin.TabularInline):
     model = CourseAccessCollection   
 
 
 
-
 class CollectionAdmin(admin.ModelAdmin):
-  list_display = ('title', 'total_weeks')
+  list_display = ('title', 'description','total_weeks', 'jms')
   list_editable = ('total_weeks',)
   inlines = [CollectionCourseInline]
+
+  def jms(self, obj):
+    item = [''] if obj.topics is None else obj.topics.split(',')
+    print(type(item))
+    return item[0]
 
 class CourseAdmin(admin.ModelAdmin):
   formfield_overrides = {
     models.TextField: { 'widget': AdminMartorWidget },
   }
-  list_display = ('title', 'description')
-  list_editable = ('description',)
+  list_display = ('title', 'tags', 'description')
+  list_editable = ('tags',)
   inlines = [CourseSectionInline]
 
 class SectionAdmin(admin.ModelAdmin):
   formfield_overrides = {
     models.TextField: { 'widget': AdminMartorWidget },
   }
-  list_display = ('title', 'description', 'short_description')
-  list_editable = ('description', 'short_description')
+  list_display = ('title', 'tags', 'short_description', 'description')
+  list_editable = ('tags', 'short_description')
   inlines = [SectionChapterInline]
 
 class ChapterAdmin(admin.ModelAdmin):
   formfield_overrides = {
     models.TextField: { 'widget': AdminMartorWidget },
   }
-  list_display = ('title', 'group', 'points_value', 'mandatory')
-  list_editable = ('group', 'points_value', 'mandatory')
-
+  list_display = ('title', 'tags', 'group', 'points_value', 'mandatory', 'description')
+  list_editable = ('tags', 'group', 'points_value', 'mandatory')
 
 class CourseAccessAdmin(admin.ModelAdmin):
   list_display = ('name',)
