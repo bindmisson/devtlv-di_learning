e1eb4014f4f3cbbd2d8592c2ccf97902c7e3b715
Jonathan <jonathan.spiller@gmail.com>
2020-08-04 08:20:32 +0300

    Adds program to Profile admin

--- di_platform/profile_app/admin.py
+++ di_platform/profile_app/admin.py
@@ -3,23 +3,34 @@ from profile_app.models import Profile, Program
 from courses_app.models import Collection, Chapter
 
 
-class ProfileData(admin.ModelAdmin):
-    readonly_fields = ('completion_percent', )
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'full_name', 'program')
+    list_editable = ('program',)
 
-    def completion_percent(self, obj):
-        completion = ''
-        # for collection in Collection.objects.all():
-        #     all_chapters = Chapter.objects.filter(
-        #         section__course__collection=collection)
-        #     done = [chapter for chapter in obj.done_chapters.all()
-        #             if chapter in all_chapters]
-        #     if len(all_chapters) == 0:
-        #         continue
-        #     else:
-        #         completion += f'{collection.title} : {(len(done)/len(all_chapters))*100}% completed \n'
+    def username(self, obj):
+        return f"{obj.user.username}"
 
-        return completion
+    def full_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
 
-
-admin.site.register(Profile, ProfileData)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Program)
+
+# class ProfileData(admin.ModelAdmin):
+#     readonly_fields = ('completion_percent', )
+
+#     def completion_percent(self, obj):
+#         completion = ''
+#         # for collection in Collection.objects.all():
+#         #     all_chapters = Chapter.objects.filter(
+#         #         section__course__collection=collection)
+#         #     done = [chapter for chapter in obj.done_chapters.all()
+#         #             if chapter in all_chapters]
+#         #     if len(all_chapters) == 0:
+#         #         continue
+#         #     else:
+#         #         completion += f'{collection.title} : {(len(done)/len(all_chapters))*100}% completed \n'
+
+#         return completion
+
+# admin.site.register(Profile, ProfileData)
