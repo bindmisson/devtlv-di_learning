2c8804fb54c379f07567dc5de0f325f73f9f6b89
BuckytheBoss <buchman.avi@gmail.com>
2020-10-22 19:50:10 +0300

    adding password reset options

--- di_platform/profile_app/views.py
+++ di_platform/profile_app/views.py
@@ -4,11 +4,16 @@ from django.contrib import messages
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
-from profile_app.models import Profile, CODE_FORMATTING_CHOICES
-from profile_app.forms import SignupForm, LoginForm, ProfileEditForm, UserEditForm, ProfilePictureUploadForm
+from .models import Profile, CODE_FORMATTING_CHOICES
+from .forms import SignupForm, LoginForm, ProfileEditForm, UserEditForm, ProfilePictureUploadForm
 from courses_app.models import CourseAccess
 from django.contrib.messages import get_messages
 from di_platform.settings import LOGIN_URL
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_text
+from django.contrib.auth.password_validation import validate_password
+from .mailers import reset_password_email
+from .tokens import account_activation_token
 
 
 @login_required(login_url=LOGIN_URL)
@@ -146,3 +151,60 @@ def login_auth(request):
 def logout_auth(request):
   logout(request)
   return redirect('profile_app:login')
+
+
+def password_change_error(request, message='Sorry, something went wrong, please try again.'):
+  messages.add_message(request, messages.ERROR, message)
+  return redirect('profile_app:password_reset_final')
+
+
+def reset_password_step1(request):
+  if request.method == "POST":
+    try:
+      user = User.objects.get(email=request.POST.get('email'))
+
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+      messages.add_message(request, messages.ERROR, 'Email does not exist in our system.')
+      return redirect('profile_app:start_password_reset')
+
+    reset_password_email(request, user)
+    return render(request, 'check_your_inbox.html')
+
+  return render(request, 'email_reset_step_one.html')
+
+
+def reset_password_step2(request, uidb64, token):
+  try:
+    uid = force_text(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=uid)
+  except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    user = None
+
+  if user is not None and account_activation_token.check_token(user, token):
+    request.session['user_pk'] = user.pk
+    return redirect('profile_app:password_reset_final')
+  else:
+    messages.add_message(request, messages.ERROR, 'Seems like something went wrong, try again!')
+    return redirect('profile_app:start_password_reset')
+
+
+def reset_password_step3(request):
+  if request.method == "POST":
+    password = request.POST.get('password')
+    user = User.objects.get(pk=request.session['user_pk'])
+
+    try:
+      password == request.POST.get('confirmpassword')
+    except:
+      return password_change_error(request=request, message='Passwords did not match.')
+
+    try:
+      validate_password(password, user=User)
+    except:
+      return password_change_error(request=request, message='Password did not meet minimum security requirements.')
+
+    user.set_password(password)
+    user.save()
+    return redirect('profile_app:login')
+
+  return render(request, 'reset_password_final.html')
